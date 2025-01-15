127f6e7ef8e1b804fc2de18f0e65405d25fc17af
zhuowenhao <zhuowenhao@huya.com>
2025-01-15 14:48:14 +0800

    feat(node): 增加运行重试机制以提高稳定性

--- app/nodes/FANode_LLM_inference.py
+++ app/nodes/FANode_LLM_inference.py
@@ -146,95 +146,120 @@ class FANode_LLM_inference(FATaskNode):
         return NotGiven
 
     async def run(self) -> List[FANodeUpdateData]:
-        try:
-            node_payloads = self.data.getContent("payloads")
-            node_results = self.data.getContent("results")
-            D_VARSINPUT: VFNodeContentData = node_payloads.byId["D_VARSINPUT"]
-            D_MODELCONFIG: VFNodeContentData = node_payloads.byId["D_MODELCONFIG"]
-            D_PROMPTS: VFNodeContentData = node_payloads.byId["D_PROMPTS"]
-            D_ANSWER: VFNodeContentData = node_results.byId["D_ANSWER"]
-            D_MODEL: VFNodeContentData = node_results.byId["D_MODEL"]
-            D_IN_TOKEN: VFNodeContentData = node_results.byId["D_IN_TOKEN"]
-            D_OUT_TOKEN: VFNodeContentData = node_results.byId["D_OUT_TOKEN"]
-            D_STOP_REASON: VFNodeContentData = node_results.byId["D_STOP_REASON"]
-            InputArgs = {}
-            for var_dict in D_VARSINPUT.data.value:
-                var = Single_VarInput.model_validate(var_dict)
-                InputArgs[var.key] = await self.getVar(var)
-            model_cfg = LLMModelConfig.model_validate(D_MODELCONFIG.data.value)
-            completions_params = {
-                "model": await self.getConfigVar(model_cfg.model),
-                "stream": model_cfg.stream,
-                "max_tokens": await self.getConfigVar(model_cfg.max_tokens),
-                "temperature": await self.getConfigVar(model_cfg.temperature),
-                "top_p": await self.getConfigVar(model_cfg.top_p),
-                # "top_k": await self.getConfigVar(model_cfg.top_k),
-                "frequency_penalty": await self.getConfigVar(
-                    model_cfg.frequency_penalty
-                ),
-            }
-            if model_cfg.stream:
-                completions_params["stream_options"] = {"include_usage": True}
+        for try_count in range(5):
+            try:
+                node_payloads = self.data.getContent("payloads")
+                node_results = self.data.getContent("results")
+                D_VARSINPUT: VFNodeContentData = node_payloads.byId["D_VARSINPUT"]
+                D_MODELCONFIG: VFNodeContentData = node_payloads.byId["D_MODELCONFIG"]
+                D_PROMPTS: VFNodeContentData = node_payloads.byId["D_PROMPTS"]
+                D_ANSWER: VFNodeContentData = node_results.byId["D_ANSWER"]
+                D_MODEL: VFNodeContentData = node_results.byId["D_MODEL"]
+                D_IN_TOKEN: VFNodeContentData = node_results.byId["D_IN_TOKEN"]
+                D_OUT_TOKEN: VFNodeContentData = node_results.byId["D_OUT_TOKEN"]
+                D_STOP_REASON: VFNodeContentData = node_results.byId["D_STOP_REASON"]
+                InputArgs = {}
+                for var_dict in D_VARSINPUT.data.value:
+                    var = Single_VarInput.model_validate(var_dict)
+                    InputArgs[var.key] = await self.getVar(var)
+                model_cfg = LLMModelConfig.model_validate(D_MODELCONFIG.data.value)
+                completions_params = {
+                    "model": await self.getConfigVar(model_cfg.model),
+                    "stream": model_cfg.stream,
+                    "max_tokens": await self.getConfigVar(model_cfg.max_tokens),
+                    "temperature": await self.getConfigVar(model_cfg.temperature),
+                    "top_p": await self.getConfigVar(model_cfg.top_p),
+                    # "top_k": await self.getConfigVar(model_cfg.top_k),
+                    "frequency_penalty": await self.getConfigVar(
+                        model_cfg.frequency_penalty
+                    ),
+                }
+                if model_cfg.stream:
+                    completions_params["stream_options"] = {"include_usage": True}
+                    pass
+                isJson = await self.getConfigVar(model_cfg.response_format) == "json"
+                if isJson:
+                    completions_params["response_format"] = {"type": "json_object"}
+                # messages
+                messages = []
+                for prompt in D_PROMPTS.data.value:
+                    prompt_obj = Single_Prompt.model_validate(prompt)
+                    prompt_obj.content = replace_vars(prompt_obj.content, InputArgs)
+                    messages.append(json.loads(prompt_obj.model_dump_json()))
+                    pass
+                completions_params["messages"] = messages
+                completions_params = {
+                    k: v for k, v in completions_params.items() if v is not NotGiven
+                }
+                chat_completion: ChatCompletion = await AsyncOAIClient.with_options(
+                    max_retries=10
+                ).chat.completions.create(**completions_params)
+                D_ANSWER.data.value = ""
+                if model_cfg.stream:
+                    async for chunk in chat_completion:
+                        chunk = cast(ChatCompletionChunk, chunk)
+                        if len(chunk.choices) > 0:
+                            content = chunk.choices[0].delta.content
+                            D_ANSWER.data.value += content
+                            D_STOP_REASON.data.value = chunk.choices[0].finish_reason
+                            pass
+                        if chunk.usage is not None:
+                            D_IN_TOKEN.data.value = chunk.usage.prompt_tokens
+                            D_OUT_TOKEN.data.value = chunk.usage.completion_tokens
+                            pass
+                else:
+                    D_ANSWER.data.value = chat_completion.choices[0].message.content
+                    D_IN_TOKEN.data.value = chat_completion.usage.prompt_tokens
+                    D_OUT_TOKEN.data.value = chat_completion.usage.completion_tokens
+                    D_STOP_REASON.data.value = chat_completion.choices[0].finish_reason
+                    pass
+                D_MODEL.data.value = completions_params["model"]
+                logger.info(
+                    f"补全Tokens：{D_IN_TOKEN.data.value} + {D_OUT_TOKEN.data.value}"
+                )
+                if isJson:
+                    json.loads(D_ANSWER.data.value)
+                self.setAllOutputStatus(FANodeStatus.Success)
+                return
+            except json.JSONDecodeError:
+                if try_count >= 5:
+                    raise Exception(f"JSON格式错误：{D_ANSWER.data.value}")
+                else:
+                    logger.warning(f"正在重试，因为JSON格式错误：{D_ANSWER.data.value}")
+                    await asyncio.sleep(2**try_count)
+                    continue
                 pass
-            if await self.getConfigVar(model_cfg.response_format) == "json":
-                completions_params["response_format"] = {"type": "json_object"}
-            # messages
-            messages = []
-            for prompt in D_PROMPTS.data.value:
-                prompt_obj = Single_Prompt.model_validate(prompt)
-                prompt_obj.content = replace_vars(prompt_obj.content, InputArgs)
-                messages.append(json.loads(prompt_obj.model_dump_json()))
+            except openai.APIConnectionError as e:
+                errmsg = traceback.format_exc()
+                if try_count >= 5:
+                    raise Exception(f"LLM节点运行失败：{errmsg}")
+                else:
+                    logger.warning(f"正在重试，因为API连接错误：{errmsg}")
+                    await asyncio.sleep(2**try_count)
+                    continue
                 pass
-            completions_params["messages"] = messages
-            completions_params = {
-                k: v for k, v in completions_params.items() if v is not NotGiven
-            }
-            chat_completion: ChatCompletion = await AsyncOAIClient.with_options(
-                max_retries=10
-            ).chat.completions.create(**completions_params)
-            D_ANSWER.data.value = ""
-            if model_cfg.stream:
-                async for chunk in chat_completion:
-                    chunk = cast(ChatCompletionChunk, chunk)
-                    if len(chunk.choices) > 0:
-                        content = chunk.choices[0].delta.content
-                        D_ANSWER.data.value += content
-                        D_STOP_REASON.data.value = chunk.choices[0].finish_reason
-                        pass
-                    if chunk.usage is not None:
-                        D_IN_TOKEN.data.value = chunk.usage.prompt_tokens
-                        D_OUT_TOKEN.data.value = chunk.usage.completion_tokens
-                        pass
-            else:
-                D_ANSWER.data.value = chat_completion.choices[0].message.content
-                D_IN_TOKEN.data.value = chat_completion.usage.prompt_tokens
-                D_OUT_TOKEN.data.value = chat_completion.usage.completion_tokens
-                D_STOP_REASON.data.value = chat_completion.choices[0].finish_reason
+            except openai.RateLimitError as e:
+                errmsg = traceback.format_exc()
+                if try_count >= 5:
+                    raise Exception(f"LLM节点运行失败：{errmsg}")
+                else:
+                    logger.warning(f"正在重试，因为API请求频率限制：{errmsg}")
+                    await asyncio.sleep(2**try_count)
+                    continue
                 pass
-            D_MODEL.data.value = completions_params["model"]
-            logger.info(
-                f"补全Tokens：{D_IN_TOKEN.data.value} + {D_OUT_TOKEN.data.value}"
-            )
-            self.setAllOutputStatus(FANodeStatus.Success)
-        except openai.APIConnectionError as e:
-            logger.error("The server could not be reached")
-            logger.error(e.__cause__)  # an underlying Exception, likely raised within httpx.
-            errmsg = traceback.format_exc()
-            raise Exception(f"LLM节点运行失败：{errmsg}")
-        except openai.RateLimitError as e:
-            logger.error("A 429 status code was received; we should back off a bit.")
-            errmsg = traceback.format_exc()
-            raise Exception(f"LLM节点运行失败：{errmsg}")
-        except openai.APIStatusError as e:
-            logger.error("Another non-200-range status code was received")
-            logger.error(e.status_code)
-            logger.error(e.response)
-            errmsg = traceback.format_exc()
-            raise Exception(f"LLM节点运行失败：{errmsg}")
-        except Exception as e:
-            errmsg = traceback.format_exc()
-            raise Exception(f"LLM节点运行失败：{errmsg}")
-        pass
+            except openai.APIStatusError as e:
+                errmsg = traceback.format_exc()
+                if try_count >= 5:
+                    raise Exception(f"LLM节点运行失败：{errmsg}")
+                else:
+                    logger.warning(f"正在重试，因为API状态错误：{errmsg}")
+                    await asyncio.sleep(2**try_count)
+                    continue
+                pass
+            except Exception as e:
+                errmsg = traceback.format_exc()
+                raise Exception(f"LLM节点运行失败：{errmsg}")
+            pass
 
     @staticmethod
     def getNodeConfig():
